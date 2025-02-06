9459c90e216e46bbc14b94a3e1babe958e3a6704
Anushya <anushya.vp19@gmail.com>
2025-02-06 17:04:54 +0530

    message

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id"{
 output "az_info" {
   value = data.aws_availability_zones.available
 }
+
+output "default_vpc_info"{
+    value = data.aws_vpc.default
+}
+
+output "mainroutetableinfo" {
+  value = data.aws_route_table.main
+}
--- variables.tf
+++ variables.tf
@@ -71,4 +71,22 @@ variable "nat_gateway_tags" {
   default = {}
 }
 
+variable "public_route_table_tags" {
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  default = {}
+}
+
+variable "database_route_table_tags" {
+  default = {}
+}
+
+variable "is_peering_required" {
+
+    type = bool
+    default = false
+  
+}
 
